54297c68cf05e83e7aa0b8046383b55fd60119ab
vivek <vivan100123@gmail.com>
2026-07-03 16:54:40 +0530

    All routes are updated here.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,53 +1,69 @@
-import { BrowserRouter, Routes, Route }
-from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import AdminDashboard from "./pages/admin/AdminDashboard";
 
-import UserStores
-from "./pages/user/UserStores";
-
-
+import UserStores from "./pages/user/UserStores";
+import StoresPage from "./pages/admin/StoresPage";
+import UsersPage from "./pages/admin/UsersPage";
 
 function App() {
-
   return (
-
     <BrowserRouter>
-
       <Routes>
+  
 
+        <Route path="/" element={<Home />} />
+
+
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
+
+ 
 
         <Route
-          path="/"
-          element={<Home />}
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
         />
 
         <Route
-          path="/login"
-          element={<Login />}
+          path="/admin/stores"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <StoresPage />
+            </ProtectedRoute>
+          }
         />
 
         <Route
-          path="/signup"
-          element={<Signup />}
+          path="/admin/users"
+          element={
+            <ProtectedRoute allowedRoles={["ADMIN"]}>
+              <UsersPage />
+            </ProtectedRoute>
+          }
         />
-
-     
-
 
         <Route
           path="/stores"
-          element={<UserStores />}
+          element={
+            <ProtectedRoute allowedRoles={["USER"]}>
+              <UserStores />
+            </ProtectedRoute>
+          }
         />
-
       </Routes>
-
     </BrowserRouter>
-
   );
-
 }
 
 export default App;

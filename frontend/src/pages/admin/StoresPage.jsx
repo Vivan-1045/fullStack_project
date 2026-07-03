b8d6645140c26804e14108191c67fcb5cb1a783f
vivek <vivan100123@gmail.com>
2026-07-04 02:44:21 +0530

    fix the store page the issue is mainly due to mismathc of bracket{}

--- frontend/src/pages/admin/StoresPage.jsx
+++ frontend/src/pages/admin/StoresPage.jsx
@@ -1,47 +1,71 @@
-import { useEffect, useState }
-from "react";
+import { useEffect, useState } from "react";
 
 import API from "../../services/api";
 
-import Navbar
-from "../../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 
 
 function StoresPage() {
 
-  const [stores, setStores] =
-    useState([]);
+  const [stores, setStores] = useState([]);
+  const [formData, setFormData] =
+    useState({
+      name: "",
+      email: "",
+      address: "",
+      owner_id: ""
+    });
 
-
-
+  const [owners, setOwners] = useState([]);
+  const [search, setSearch] = useState("");
 
   const fetchStores = async () => {
-
     try {
-
-      const res = await API.get(
-        "/admin/stores"
-      );
-
+      const res = await API.get(`/admin/stores?name=${search}`);
       setStores(res.data);
 
-    } catch (error) {
-
-      console.log(error);
-
+    } catch (err) {
+      console.log(err);
     }
-
   };
 
+  const fetchOwners = async () => {
+    try {
+      const res = await API.get("/admin/users?role=OWNER");
+      setOwners(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const createStore = async (e) => {
+    e.preventDefault();
+
+    try {
+      await API.post("/admin/stores", formData);
+      alert("Store created successfully!");
+      fetchStores();
+    } catch (error) {
+      {
+        alert(error.response?.data?.message);
+      }
+    };
+
+  }
 
   useEffect(() => {
-
     fetchStores();
+    fetchOwners();
 
-  }, []);
-
+  }, [search]);
 
 
 
@@ -60,9 +84,137 @@ function StoresPage() {
             mb-6
           "
         >
+
           Stores
         </h1>
 
+        {owners.length === 0 && (
+
+          <div
+            className="
+      bg-yellow-100
+      text-yellow-800
+      p-4
+      rounded
+      mb-6
+    "
+          >
+            No owners found.
+
+            Please create an OWNER user first
+            from Users page.
+          </div>
+
+        )}
+
+        <form
+          onSubmit={createStore}
+          className="
+    bg-white
+    shadow-md
+    p-6
+    rounded
+    mb-8
+    grid
+    grid-cols-2
+    gap-4
+  "
+        >
+
+          <input
+            type="text"
+            name="name"
+            placeholder="Store Name"
+            onChange={handleChange}
+            className="border p-3 rounded"
+            required
+          />
+
+
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Store Email"
+            onChange={handleChange}
+            required
+            className="border p-3 rounded"
+          />
+
+
+
+          <input
+            type="text"
+            name="address"
+            placeholder="Address"
+            onChange={handleChange}
+            className="border p-3 rounded"
+            required
+            minLength={8}
+          />
+
+
+
+          <select
+            name="owner_id"
+            onChange={handleChange}
+            className="border p-3 rounded"
+            required
+          >
+
+            <option>
+              Select Owner
+            </option>
+
+            {owners.map((owner) => (
+
+              <option
+                key={owner.id}
+                value={owner.id}
+              >
+                {owner.name}
+              </option>
+
+            ))}
+
+          </select>
+
+
+
+          <button
+            type="submit"
+            disabled={owners.length === 0}
+            className={`
+    text-white
+    rounded
+    p-3
+
+    ${owners.length === 0
+                ? "bg-gray-400 cursor-not-allowed"
+                : "bg-green-600"
+              }
+  `}
+          >
+            Create Store
+          </button>
+
+        </form>
+
+        <input
+          type="text"
+          placeholder="Search store"
+          value={search}
+          onChange={(e) =>
+            setSearch(e.target.value)
+          }
+          className="
+    border
+    p-3
+    rounded
+    mb-4
+    w-full
+  "
+        />
 
 
         <table

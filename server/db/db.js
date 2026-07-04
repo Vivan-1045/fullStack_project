cc1857b83dd234aefff29659dd01cb7d2ac10243
vivek <vivan100123@gmail.com>
2026-07-04 11:02:00 +0530

    debugging

--- server/db/db.js
+++ server/db/db.js
@@ -4,8 +4,14 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
 });
+
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_PORT);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_NAME);
 
 connection.connect((err) =>{
     if(err){
